d502c97882a71ca9960b4536aa88d962fd3a0d4d
mhambali920 <mhambali920@gmail.com>
2025-11-28 19:27:25 +0700

    update button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,12 @@
 import { type ButtonHTMLAttributes } from "react";
 import { cn } from "../../lib/utils";
 import { buttonVariants, type ButtonVariantProps } from "./button-variants";
+import { Slot } from "@radix-ui/react-slot";
 
 interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     ButtonVariantProps {
-  loading?: boolean;
+  asChild?: boolean;
   color?:
     | "primary"
     | "secondary"
@@ -23,42 +24,17 @@ export const Button = ({
   size,
   color,
   className,
-  children,
-  loading,
   disabled,
+  asChild = false,
   ...props
 }: ButtonProps) => {
-  return (
-    <button
-      className={cn(buttonVariants({ variant, size, color }), className)}
-      disabled={disabled || loading}
-      {...props}
-    >
-      {loading && (
-        <svg
-          className="animate-spin -ml-1 mr-2 h-4 w-4"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          />
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 
-            1.135 5.824 3 7.938l3-2.647z"
-          />
-        </svg>
-      )}
+  const Comp = asChild ? Slot : "button";
 
-      {children}
-    </button>
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, color }), className)}
+      disabled={disabled}
+      {...props}
+    />
   );
 };
