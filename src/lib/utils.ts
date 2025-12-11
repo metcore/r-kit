eca4c31adca0c3cb4c4f40285407dda3807fdf87
mhambali920 <mhambali920@gmail.com>
2025-12-11 10:31:40 +0700

    feat: utility check error message

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function fieldHasError(errorMessages: unknown) {
+  return typeof errorMessages === "string"
+    ? errorMessages.trim().length > 0
+    : Array.isArray(errorMessages)
+      ? errorMessages.length > 0
+      : false;
+}
