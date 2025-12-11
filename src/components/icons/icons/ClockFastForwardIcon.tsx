6a1476c303327660afdf8fb3f7479ac16f49b216
mhambali920 <mhambali920@gmail.com>
2025-12-11 10:32:32 +0700

    fix color ClockFastForwardIcon

--- src/components/icons/icons/ClockFastForwardIcon.tsx
+++ src/components/icons/icons/ClockFastForwardIcon.tsx
@@ -10,15 +10,14 @@ const ClockFastForwardIcon: React.FC<IconProps> = ({
   color = "currentColor",
   ...props
 }) => (
-  <svg
-    width={size}
-    height={size}
-    fill={color}
-    viewBox="0 0 24 24"
-    fill="none"
-    {...props}
-  >
-    <path stroke="#484848" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m22.7 11.5-2 2-2-2m2.245 1.5A9 9 0 1 0 19 17.657M12 7v5l3 2"/>
+  <svg width={size} height={size} viewBox="0 0 24 24" fill="none" {...props}>
+    <path
+      stroke={color}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="m22.7 11.5-2 2-2-2m2.245 1.5A9 9 0 1 0 19 17.657M12 7v5l3 2"
+    />
   </svg>
 );
 
