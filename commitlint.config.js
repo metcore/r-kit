15ed94a8dfa408335087570f718c5b8c2f6f8d17
Slamet RIyadi <slametriyadi@MacBook-Air-Slamet.local>
2026-03-17 14:04:26 +0700

    feat: updated

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,3 @@
-module.exports = {
+export default {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',   
-        'chore',   
-        'ci',      
-        'docs',    
-        'feat',    
-        'fix',     
-        'perf',    
-        'refactor',
-        'revert',  
-        'style',   
-        'test',    
-      ],
-    ],
-    'subject-case': [0],
-  },
 };
