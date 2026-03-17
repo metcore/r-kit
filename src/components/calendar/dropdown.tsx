9858dfdf034894616d32d070f229f5758a215e4e
Slamet RIyadi <slametriyadi@MacBook-Air-Slamet.local>
2026-03-17 15:33:55 +0700

    fix: eslitn standard

--- src/components/calendar/dropdown.tsx
+++ src/components/calendar/dropdown.tsx
@@ -1,13 +1,13 @@
-import { cn } from "../../lib/utils";
-import { Button } from "../button";
-import { Icon } from "../icons";
-import { Text } from "../text";
-import type { ButtonDropdownProps } from "./type";
+import { cn } from '../../lib/utils';
+import { Button } from '../button';
+import { Icon } from '../icons';
+import { Text } from '../text';
+import type { ButtonDropdownProps } from './type';
 
 const ButtonDropdown = ({
   onClick,
   active,
-  size = "md",
+  size = 'md',
 }: ButtonDropdownProps) => {
   const size_map = {
     sm: 11,
@@ -16,17 +16,20 @@ const ButtonDropdown = ({
   };
   return (
     <Button
-      size={"icon"}
+      size={'icon'}
       onClick={onClick}
       color="gray"
       className={cn(
-        "bg-transparent transition-colors outline-none *:duration-300 hover:bg-gray-50 focus:outline-none",
+        'bg-transparent transition-colors outline-none *:duration-300 hover:bg-gray-50 focus:outline-none'
       )}
     >
       <Icon
         name="arrow-down"
         size={size_map[size]}
-        className={cn("text-primary-1000", active ? "rotate-180" : "rotate-0")}
+        className={cn(
+          'text-primary-1000',
+          active == true ? 'rotate-180' : 'rotate-0'
+        )}
       />
     </Button>
   );
@@ -45,10 +48,10 @@ const ItemDropdown = ({
     <button
       onClick={onClick}
       className={cn(
-        "w-full min-w-23.5 cursor-pointer rounded-md border px-1.5 py-1 text-sm transition-colors outline-none",
+        'w-full min-w-23.5 cursor-pointer rounded-md border px-1.5 py-1 text-sm transition-colors outline-none',
         active
-          ? "bg-primary-50 border-primary-300"
-          : "border-transparent hover:bg-gray-50",
+          ? 'bg-primary-50 border-primary-300'
+          : 'border-transparent hover:bg-gray-50'
       )}
     >
       <Text value={value} weight="medium" />
