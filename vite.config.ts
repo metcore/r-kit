47e6d3960d63cefc7a44eefce5204947e589716f
Slamet RIyadi <slametriyadi@MacBook-Air-Slamet.local>
2026-06-13 12:42:39 +0700

    fix: build docs

--- vite.config.ts
+++ vite.config.ts
@@ -12,34 +12,6 @@ export default defineConfig(({ mode }) => {
     server: {
       allowedHosts: env?.BASE_URL ? env.BASE_URL.split(',') : [],
     },
-
-    build: {
-      // entry
-      lib: {
-        entry: resolve(__dirname, 'src/index.ts'),
-        name: 'r-kit',
-        fileName: (format) => `index.${format}.js`,
-      },
-      rollupOptions: {
-        // make sure to externalize deps that shouldn't be bundled
-        // into your library
-        external: [
-          'react',
-          'react-dom',
-          'class-variance-authority',
-          'clsx',
-          'tailwind-merge',
-        ],
-        output: {
-          // Provide global variables to use in the UMD build
-          // Add external deps here
-          globals: {
-            'react': 'React',
-            'react-dom': 'ReactDOM',
-            'class-variance-authority': 'CVA',
-          },
-        },
-      },
-    },
+    build: { outDir: 'dist' },
   };
 });
